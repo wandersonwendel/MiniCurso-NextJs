380b6a0ad43ce0b977e71a8271550260513a486f
wandersonwendel <wandsolopes@alu.ufc.br>
2024-07-10 10:36:00 -0300

    feat: Create variables in the global scope. - Define 'nome' as a constant and 'canal' as a mutable variable.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,19 @@
+const nome = "Wanderson";
+let canal = "CFBCursos";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col justify-center items-center"></main>
+    <main className="flex min-h-screen flex-col justify-center items-center">
+      <div>
+        {/* Utilizando o Js dentro do HTML, para acessar variáveis */}
+        <div>{nome}</div>
+        <div>{canal}</div>
+
+        <div>Minicurso de Next.js</div>
+        <div>Next.js 14</div>
+        <div>TypeScript</div>
+        <div>React</div>
+      </div>
+    </main>
   );
 }
